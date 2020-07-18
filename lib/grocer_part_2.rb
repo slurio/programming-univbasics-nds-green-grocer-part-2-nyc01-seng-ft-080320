05e2820e235c1a433a119a661ac062990b229001
Samantha Lurio <samantha.lurio@gmail.com>
2020-07-18 17:13:12 +0000

    Automatically backed up by Learn

--- lib/grocer_part_2.rb
+++ lib/grocer_part_2.rb
@@ -53,11 +53,11 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
+  binding.pry
   grand_total = 0
   checkout_items = []
   checkout_items = consolidate_cart(cart)
   checkout_items = apply_coupons(checkout_items, coupons)
-  binding.pry
   checkout_items = apply_clearance(checkout_items)
   checkout_items.each do |item|
     grand_total += (item[:price] * item[:count])
